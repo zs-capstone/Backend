61aa33d7bffe434dabcba7e724f960afeefa67aa
idwooin <idwooin@naver.com>
2022-11-16 16:57:35 +0900

    [main] chore

--- src/main/java/uos/capstone/backend/BackendApplication.java
+++ src/main/java/uos/capstone/backend/BackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class BackendApplication {
-
+	// 시작
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
