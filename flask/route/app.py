c66b7bc7e64895c0b160b94dc315c664bb393713
idwooin <idwooin@naver.com>
2022-11-26 21:54:33 +0900

    [main] refactor: 일시 주석 처리

--- flask/route/app.py
+++ flask/route/app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def index():
     return "ai 정상 작동"
 
-@app.route('/ai', methods=['GET'])
-def index():
-    return "ai 정상 작동"
-
-@app.route('/ai', methods=['GET'])
-def index():
-    return "ai 정상 작동"
+# @app.route('/ai', methods=['GET'])
+# def index():
+#     return "ai 정상 작동"
+#
+# @app.route('/ai', methods=['GET'])
+# def index():
+#     return "ai 정상 작동"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=6000)
