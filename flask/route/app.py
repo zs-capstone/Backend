f14ba251e361a331d313fb8171b698dcfc4aa357
idwooin <idwooin@naver.com>
2022-11-28 06:52:18 +0900

    [main] refactor: flask 추천 api 추가

--- flask/route/app.py
+++ flask/route/app.py
@@ -1,18 +1,71 @@
-from flask import Flask
+from flask      import Flask, request, jsonify, current_app
+from flask.json import JSONEncoder
+from sqlalchemy import create_engine, text
+from train_code.item_CF_train_code import ItemCFtrain
+import csv
 
 app = Flask(__name__)
 
+def get_star_reviews():
+    reviews = current_app.database.execute(text("""
+        SELECT 
+            username,
+            place_id,
+            rate
+        FROM place_review
+    """),{}).fetchall()
+
+    fl = []
+    for r in reviews:
+        fl.append({
+            'username'      : r['username'],
+            'place_id'    : r['place_id'],
+            'rate'   : r['rate']
+        })
+
+    with open("output.csv","w",newline="") as f:
+        title = "username,place_id,rate".split(",")
+        cw = csv.DictWriter(f,title,delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        cw.writeheader()
+        cw.writerows(fl)
+
+    return
+
 @app.route('/', methods=['GET'])
-def index():
+def health_check():
     return "ai 정상 작동"
 
-# @app.route('/ai', methods=['GET'])
-# def index():
-#     return "ai 정상 작동"
-#
-# @app.route('/ai', methods=['GET'])
-# def index():
-#     return "ai 정상 작동"
+@app.route('/eval', methods=['POST'])
+def eval():
+    data = request.get_json()
+    orglist = data['orgPlaceList']
+    username = data['username']
+    day = data['day']
+    n = data['n']
+
+    get_star_reviews()
+    ict = ItemCFtrain(username,int(day),int(n))
+    a = ict.start()
+
+    ex = []
+    for org in orglist:
+        if org not in a:
+            ex.append(org)
+
+    a = ex + a[:len(a)-len(ex)]
+
+    fl = []
+    for elem in a:
+        fl.append({
+            "placeId": elem
+        })
+    print(jsonify({"listEvalResponse":fl}))
+    return jsonify({"listEvalResponse":fl})
 
 if __name__ == '__main__':
+    app.config.from_pyfile("./config/config.py")
+	
+    # 데이터 베이스와 연동해준다.
+    database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
+    app.database = database
     app.run(host='0.0.0.0', debug=True, port=6000)
